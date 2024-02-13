fc2984291e05e9efc051480b0372ffe7fe7cdf6f
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-13 16:40:34 +0530

    datatype

---
+++ script15.py
@@ -0,0 +1,31 @@
+# s1=(11,22,33)
+# print(type(s1))
+
+# s2={
+#     "hello"
+# }
+# print(type(s2))
+
+# s3=[11,22,33]
+# print(type(s3))
+
+# s4={11,22,33,}
+# print(type(s4))
+
+# s5={
+#     'name':'aasavari'
+# }
+# print(type(s5))
+
+# s6=(11)
+# print(type(s6))
+
+# s7=[11]
+# print(type(s7))
+
+# s8={11}
+# print(type(s8))
+
+def my_function():
+    return [1, 2, 3]
+print(my_function)
