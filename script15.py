fbfae71f6fce645ab926f2151bd852d73c79caf0
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-14 16:52:11 +0530

    set

--- script15.py
+++ script15.py
@@ -26,6 +26,81 @@
 # s8={11}
 # print(type(s8))
 
-def my_function():
-    return [1, 2, 3]
-print(my_function)
+# #1 can store unique value
+# s1={11,22,33,11,44,22}
+# print(s1)
+
+# #2 set store value by index
+# s2={11,22,33,44}
+# print(s2[1])
+
+#3 random element
+# s3={'amol','ram',11,22}
+# print(s3)
+
+# #4 verify particular value in set
+# print('ram' in s3)
+
+# #5 
+# print(len(s3))
+
+#6 can range op 
+# city=['pune','mumbai','nashik','nagpur']
+# print(type(city))
+# for x in range(4):
+#    print(x)
+#    print(city[x])
+
+# for item in city:
+#      print(item)
+
+# setc={'pune','mumbai','nashik','nagpur'}
+# for item in setc:
+#      print(item)
+
+# print(len(setc))
+
+#pgm1 add
+setA={11,22,33,55}
+setA.add(333)
+print(setA)
+
+#pgm2 remove
+setB={"ram","ramesh","raj","sham"}
+setB.remove("raj")
+print(setB)
+
+# #pgm3 pop
+# setB.pop()
+# print(setB)
+
+# #pgm4 clear
+# setB.clear()
+# print(setB)
+
+#pgm5 update
+# setB.update(['sarika','sam'])
+# print(setB)
+
+#pgm6 
+# setB=['ram','ramesh','sham','ovi']
+# setc=setB
+# print(setc)
+# setc.remove("ram")
+# print(setc)
+
+# setE=setB.copy()
+# setE.remove("sham")
+# print(setE)
+# print(setB)
+
+#pgm7 
+setD={11,22,33}
+setF={44,55,66}
+setK=setD.union(setF)
+print(setK)
+
+setW={11,22,33,44}
+setQ={33,44,55,66}
+setR=setW.intersection(setQ)
+print(setR)
